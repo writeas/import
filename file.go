54e3e179432840c929441ce83a8c6d17cbd20127
Rob Loranger <dev@loranger.xyz>
2019-08-15 10:38:54 -0700

    add library functions for text/markdown files
    this adds functions for reading and parsing a file from disk into a
    writeas.PostParams.
    
    there are wrappers over the main logic to parse all files in a directory
    path, with or without a regular expression to match.

---
+++ file.go
@@ -0,0 +1,105 @@
+package wfimport
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/writeas/go-writeas"
+)
+
+// FromDirectoryMatch reads all text and markdown files in path that match the
+// pattern returning the parsed posts and an error if any.
+//
+// The pattern should be a valid regex, for more details see
+// https://golang.org/s/re2syntax or run `go doc regexp/syntax`
+func FromDirectoryMatch(path, pattern string) ([]*writeas.PostParams, error) {
+	return fromDirectory(path, pattern)
+}
+
+// FromDirectory reads all text and markdown files in path and returns the
+// parsed posts and an error if any.
+func FromDirectory(path string) ([]*writeas.PostParams, error) {
+	return fromDirectory(path, "")
+}
+
+// fromDirectory takes an 'optional' pattern, if an empty string is passed
+// all valid txt and md files will be included under path.
+// Otherwise pattern should be a valid regex per MatchFromDirectory
+func fromDirectory(path, pattern string) ([]*writeas.PostParams, error) {
+	if pattern == "" {
+		pattern = "."
+	}
+	rx, err := regexp.Compile(pattern)
+	if err != nil {
+		return nil, err
+	}
+	list, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+	if len(list) == 0 {
+		return nil, fmt.Errorf("directory was empty")
+	}
+
+	var postErrors error
+	posts := []*writeas.PostParams{}
+	for _, f := range list {
+		if !f.IsDir() {
+			filename := f.Name()
+			if rx.MatchString(filename) && (strings.HasSuffix(filename, ".txt") || strings.HasSuffix(filename, ".md")) {
+				post, err := FromFile(filepath.Join(path, filename))
+				if err != nil {
+					postErrors = multierror.Append(postErrors, err)
+					continue
+				}
+
+				posts = append(posts, post)
+			}
+		}
+	}
+	return posts, postErrors
+}
+
+// FromFile reads in a file from path and returns the parsed post and an error
+// if any. The title will be extracted from the first markdown level 1 header.
+//
+// Warning: FromFile will read any file provided, however only plain text or
+// markdown will be parsed.
+func FromFile(path string) (*writeas.PostParams, error) {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	if len(b) == 0 {
+		return nil, fmt.Errorf("file was empty")
+	}
+
+	// TODO: should title be the filename when no title was extracted?
+	title, body := extractTitle(string(b))
+	post := writeas.PostParams{
+		Title:   title,
+		Content: body,
+	}
+
+	return &post, nil
+}
+
+// TODO: copied from writeas/web-core/posts due to errors with package imports
+// maybe also find a way to grab the first line as a title in plain text files
+func extractTitle(content string) (title string, body string) {
+	if hashIndex := strings.Index(content, "# "); hashIndex == 0 {
+		eol := strings.IndexRune(content, '\n')
+		// First line should start with # and end with \n
+		if eol != -1 {
+			body = strings.TrimLeft(content[eol:], " \t\n\r")
+			title = content[len("# "):eol]
+			return
+		}
+	}
+	body = content
+	return
+}
---
+++ file_test.go
@@ -0,0 +1,266 @@
+package wfimport
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestFromDirectory(t *testing.T) {
+	testDir := "test"
+	files := []string{"test.md", "test2.txt", "test3"}
+	dirs := []string{"one", "two.md"}
+
+	// set up test directory and children
+	err := os.Mkdir(testDir, os.ModeDir|os.ModePerm)
+	defer os.RemoveAll(testDir)
+	if err != nil {
+		t.Fatalf("failed to create base test dir: %v", err)
+	}
+	for _, fn := range files {
+		f, err := os.Create(filepath.Join(testDir, fn))
+		if err != nil {
+			t.Fatalf("failed to create test file: %v", err)
+		}
+		defer f.Close()
+
+		if strings.HasSuffix(fn, ".md") {
+			_, err = f.WriteString(fmt.Sprintf("# a title\n%s", fn))
+		} else {
+			_, err = f.WriteString(fn)
+		}
+		if err != nil {
+			t.Fatalf("failed to write test file contents: %v", err)
+		}
+	}
+	for _, dn := range dirs {
+		err := os.Mkdir(filepath.Join(testDir, dn), os.ModeDir)
+		if err != nil {
+			t.Fatalf("failed to create test dir: %v", err)
+		}
+	}
+
+	posts, err := FromDirectory(testDir)
+	if err != nil {
+		t.Fatalf("failed to parse files from directory: %v", err)
+	}
+
+	numExpected := 2 // length of files minus any without extensions
+	numResults := len(posts)
+	if numResults != numExpected {
+		t.Fatalf("post count mismatch.\bgot: %d\nexpecting: %d", numResults, numExpected)
+	}
+}
+
+func TestFromDirectoryMatch(t *testing.T) {
+	testDir := "test"
+	files := []string{"test.md", "test2.txt", "test3"}
+
+	// set up test directory and children
+	err := os.Mkdir(testDir, os.ModeDir|os.ModePerm)
+	defer os.RemoveAll(testDir)
+	if err != nil {
+		t.Fatalf("failed to create base test dir: %v", err)
+	}
+	for _, fn := range files {
+		f, err := os.Create(filepath.Join(testDir, fn))
+		if err != nil {
+			t.Fatalf("failed to create test file: %v", err)
+		}
+		defer f.Close()
+
+		_, err = f.WriteString(fn)
+		if err != nil {
+			t.Fatalf("failed to write test file contents: %v", err)
+		}
+	}
+
+	posts, err := FromDirectoryMatch(testDir, `\d+`)
+	if err != nil {
+		t.Fatalf("failed to parse files from directory: %v", err)
+	}
+
+	numExpected := 1 // length of files matching expression
+	numResults := len(posts)
+	if numResults != numExpected {
+		t.Fatalf("post count mismatch.\bgot: %d\nexpecting: %d", numResults, numExpected)
+	}
+
+	posts, err = FromDirectoryMatch(testDir, `test`)
+	if err != nil {
+		t.Fatalf("failed to parse files from directory: %v", err)
+	}
+
+	numExpected = 2 // length of files matching expression
+	numResults = len(posts)
+	if numResults != numExpected {
+		t.Fatalf("post count mismatch.\bgot: %d\nexpecting: %d", numResults, numExpected)
+	}
+}
+
+func TestFromDirectoryNoPath(t *testing.T) {
+	posts, err := FromDirectory("")
+	if err == nil {
+		t.Fatal("error is nil but should not open directory without name")
+	}
+	if posts != nil {
+		t.Fatal("posts returned but should be nil")
+	}
+}
+
+func TestFromDirectoryRelativePath(t *testing.T) {
+	testDir := "test"
+	err := os.Mkdir(testDir, os.ModeDir|os.ModePerm)
+	if err != nil {
+		t.Fatal("failed to create test dir")
+	}
+	defer os.Remove(testDir)
+
+	err = os.Chdir(testDir)
+	if err != nil {
+		t.Fatalf("failed to change into test dir: %v", err)
+	}
+	defer os.Chdir("../")
+
+	f, err := os.Create("blog.md")
+	if err != nil {
+		t.Fatalf("failed to create test file: %v", err)
+	}
+	defer func() {
+		f.Close()
+		os.Remove("blog.md")
+	}()
+	_, err = f.WriteString("hello")
+	if err != nil {
+		t.Fatalf("failed to write to test file: %v", err)
+	}
+
+	post, err := FromDirectory(".")
+	if err != nil {
+		t.Fatalf("failed to parse from relative directory: %v", err)
+	}
+
+	if post == nil {
+		t.Fatal("post is nil but should exist")
+	}
+}
+
+func TestFromDirectoryEmptyDir(t *testing.T) {
+	testDir := "test"
+	err := os.Mkdir(testDir, os.ModeDir|os.ModePerm)
+	if err != nil {
+		t.Fatal("failed to create test dir")
+	}
+	defer os.Remove(testDir)
+
+	posts, err := FromDirectory(testDir)
+	if err == nil {
+		t.Fatal("error is nil but directory was empty")
+	}
+	if posts != nil {
+		t.Fatal("posts returned but should be nil")
+	}
+}
+
+func TestFromDirectoryErrors(t *testing.T) {
+	testDir := "test"
+	files := []string{"test.md", "test2.txt", "test3"}
+
+	// set up test directory and children
+	err := os.Mkdir(testDir, os.ModeDir|os.ModePerm)
+	defer os.RemoveAll(testDir)
+	if err != nil {
+		t.Fatalf("failed to create base test dir: %v", err)
+	}
+	for _, fn := range files {
+		f, err := os.Create(filepath.Join(testDir, fn))
+		if err != nil {
+			t.Fatalf("failed to create test file: %v", err)
+		}
+		defer f.Close()
+	}
+	posts, err := FromDirectory(testDir)
+	if err == nil {
+		t.Fatal("error was nil but no files have contents")
+	}
+	if len(posts) == len(files) {
+		t.Fatal("files with errors were returned, should be skipped")
+	}
+}
+
+func TestFromFile(t *testing.T) {
+	filename := "test.txt"
+	postBody := `test post
+	
+	this is a test`
+
+	// create file to read
+	file, err := os.Create(filename)
+	if err != nil {
+		t.Fatalf("failed to create file: %v", err)
+	}
+	defer os.Remove(filename)
+
+	_, err = file.WriteString(postBody)
+	if err != nil {
+		t.Fatalf("failed to write to file: %v", err)
+	}
+	err = file.Close()
+	if err != nil {
+		t.Fatalf("failed to close file: %v", err)
+	}
+
+	// read from file
+	post, err := FromFile(filename)
+	if err != nil {
+		t.Fatalf("failed to open file: %v", err)
+	}
+	if post == nil {
+		t.Fatal("post was nil")
+	}
+	title, body := extractTitle(postBody)
+	if post.Content != body {
+		t.Logf("post content mismatch.")
+		t.Logf("got:\n%s", post.Content)
+		t.Logf("expected:\n%s", body)
+		t.FailNow()
+	}
+	if post.Title != title {
+		t.Logf("post title mismatch.")
+		t.Logf("got:\n%s", post.Title)
+		t.Logf("expected:\n%s", title)
+		t.FailNow()
+	}
+}
+
+func TestFromFileNoPath(t *testing.T) {
+	post, err := FromFile("")
+	if err == nil {
+		t.Fatal("error was nil but should not open an empty path")
+	}
+	if post != nil {
+		t.Fatal("post was returned but should be nil")
+	}
+}
+
+func TestFromFileEmptyBody(t *testing.T) {
+	testFile := "test.txt"
+	f, err := os.Create(testFile)
+	if err != nil {
+		t.Fatalf("failed to create test file: %v", err)
+	}
+	defer func() {
+		f.Close()
+		os.Remove(f.Name())
+	}()
+
+	post, err := FromFile(testFile)
+	if err == nil {
+		t.Fatalf("error was nil but should error with empty file")
+	}
+	if post != nil {
+		t.Fatal("post was returned but should be nil")
+	}
+}
