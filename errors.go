a0d4ec3a62d4a2327902e73c2e13bb8b97cd8c9d
Rob Loranger <dev@loranger.xyz>
2019-08-19 14:11:43 -0700

    add error for invalid file, comment errs

--- errors.go
+++ errors.go
@@ -3,5 +3,8 @@ package wfimport
 import "errors"
 
 var (
+	// ErrEmptyFile is returned when the file is empty
 	ErrEmptyFile = errors.New("file is empty")
+	// ErrInvalidFiletype is returned when the file was not plaintext
+	ErrInvalidFiletype = errors.New("file type is invalid")
 )
