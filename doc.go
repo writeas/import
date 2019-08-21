77536b826e5f04ede7885178404cdde809c61ff3
Rob Loranger <dev@loranger.xyz>
2019-08-21 09:32:38 -0700

    update package doc, fix formatting

--- doc.go
+++ doc.go
@@ -2,9 +2,39 @@
 Package wfimport provides library functions for parsing and importing
 writefreely posts from various files.
 
+Status
+
 Current support is for files, directories and zip archives.
 Support is planned for exported data from Medium, Ghost, Wordpress and
 writefreely exports in zip or json.
-*/
 
+About Posts
+
+In the context of this package a post is actually refering to a PostParams from
+github.com/writeas/go-writeas.
+
+	// PostParams holds values for creating or updating a post.
+	PostParams struct {
+		// Parameters only for updating
+		ID    string `json:"-"`
+		Token string `json:"token,omitempty"`
+
+		// Parameters for creating or updating
+		Slug     string     `json:"slug"`
+		Created  *time.Time `json:"created,omitempty"`
+		Updated  *time.Time `json:"updated,omitempty"`
+		Title    string     `json:"title,omitempty"`
+		Content  string     `json:"body,omitempty"`
+		Font     string     `json:"font,omitempty"`
+		IsRTL    *bool      `json:"rtl,omitempty"`
+		Language *string    `json:"lang,omitempty"`
+
+		// Parameters only for creating
+		Crosspost []map[string]string `json:"crosspost,omitempty"`
+
+		// Parameters for collection posts
+		Collection string `json:"-"`
+	}
+
+*/
 package wfimport
