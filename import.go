26e91ab4757af8694bb1a51efff4dcafc85e172a
Rob Loranger <dev@loranger.xyz>
2019-08-21 09:02:52 -0700

    add main package doc

---
+++ import.go
@@ -0,0 +1,8 @@
+// Package wfimport provides library functions for parsing and importing
+// writefreely posts from various files.
+//
+// Current support is for files, directories and zip archives.
+// Support is planned for exported data from Medium, Ghost, Wordpress and
+// writefreely exports in zip or json.
+
+package wfimport
