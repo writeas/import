4ecfd202667c5488af064ca1688bf25ce4ddf275
Rob Loranger <dev@loranger.xyz>
2020-01-08 07:39:06 -0800

    update year and add contributors mention

--- errors.go
+++ errors.go
@@ -1,4 +1,4 @@
-// Copyright © 2018-2019 A Bunch Tell LLC.
+// Copyright © 2019-2020 A Bunch Tell LLC. and contributors.
 //
 // This is free software: you can redistribute it and/or modify
 // it under the terms of the Mozilla Public License, included
